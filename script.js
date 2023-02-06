4b3c3a3d0f25db29a1769bfeb1b4ba3a6a58ba5f
ClintonYounge <clintonyounge18@gmail.com>
2023-02-06 15:33:41 -0400

    Add javascript menu code

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector('.hamburger');
+const menu = document.getElementById('theMenu')
+const xBtn = document.getElementById('xBtn');
+const navLinks = document.querySelectorAll('.navs');
+
+function openMenu() {
+    menu.style.opacity = '90%';
+}
+
+function closeMenu() {
+    menu.style.opacity = 0;
+}
+
+menuBtn.addEventListener('click', openMenu);
+navLinks[0].addEventListener('click', closeMenu);
+navLinks[1].addEventListener('click', closeMenu);
+navLinks[2].addEventListener('click', closeMenu);
+xBtn.addEventListener('click', closeMenu);
