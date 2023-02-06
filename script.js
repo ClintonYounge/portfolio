ae0a82f1b4b7362c29c3920ae497f74f710db270
ClintonYounge <clintonyounge18@gmail.com>
2023-02-06 18:06:58 -0400

    Fix linter errors

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const menuBtn = document.querySelector('.hamburger');
-const menu = document.getElementById('theMenu')
+const menu = document.getElementById('theMenu');
 const xBtn = document.getElementById('xBtn');
 const navLinks = document.querySelectorAll('.navs');
 
 function openMenu() {
-    menu.style.opacity = '90%';
+  menu.style.opacity = '90%';
 }
 
 function closeMenu() {
-    menu.style.opacity = 0;
+  menu.style.opacity = 0;
 }
 
 menuBtn.addEventListener('click', openMenu);
