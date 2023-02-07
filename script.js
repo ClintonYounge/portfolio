33e7acaa6f8f7f559e349646067fffd3f6c4cc2d
ClintonYounge <clintonyounge18@gmail.com>
2023-02-06 21:57:20 -0400

    Add blur to menu background

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ const menuBtn = document.querySelector('.hamburger');
 const menu = document.getElementById('theMenu');
 const xBtn = document.getElementById('xBtn');
 const navLinks = document.querySelectorAll('.navs');
+const introSection = document.querySelector('.introduction');
+const workSection = document.getElementById('work-section');
+const aboutSection = document.getElementById('about-section');
+const contactSection = document.getElementById('contact');
 
 function openMenu() {
   menu.style.display = 'block';
+  introSection.style.filter = 'blur(2px)';
+  workSection.style.filter = 'blur(2px)';
+  aboutSection.style.filter = 'blur(2px)';
+  contactSection.style.filter = 'blur(2px)';
 }
 
 function closeMenu() {
+  introSection.style.filter = 'none';
+  workSection.style.filter = 'none';
+  aboutSection.style.filter = 'none';
+  contactSection.style.filter = 'none';
   menu.style.display = 'none';
 }
 
