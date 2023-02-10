18b3a920385ca14f0efac7dc7c754e2111167336
ClintonYounge <clintonyounge18@gmail.com>
2023-02-10 16:10:08 -0400

    update form preservation code

--- preserve-data/preserve-data.js
+++ preserve-data/preserve-data.js
@@ -1,19 +1,19 @@
+const email = document.getElementById('user-email');
 const inputName = document.getElementById('username');
 const message = document.getElementById('text-area');
 const rawData = localStorage.getItem('cachedFormData');
-const email = document.getElementById('user-email');
 
 let formObject = {
-  name: '',
-  mail: '',
-  'text-area': '',
+  username: '',
+  message: '',
+  'user-email': '',
 };
 
 if (rawData) {
   formObject = JSON.parse(rawData);
-  inputName.value = formObject.name;
-  email.value = formObject.mail;
-  message.value = formObject['text-box'];
+  inputName.value = formObject.username;
+  email.value = formObject['user-email'];
+  text_area.value = formObject['text-area'];
 }
 
 const dataStore = (event) => {
