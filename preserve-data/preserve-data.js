29960e5bf1cd1e38f2ea9f75b736e118f1cb1236
ClintonYounge <clintonyounge18@gmail.com>
2023-02-10 14:12:27 -0400

    Fix linter errors

--- preserve-data/preserve-data.js
+++ preserve-data/preserve-data.js
@@ -1,7 +1,6 @@
 const inputName = document.getElementById('username');
 const message = document.getElementById('text-area');
 const rawData = localStorage.getItem('cachedFormData');
-const form = document.querySelector('form');
 const email = document.getElementById('user-email');
 
 let formObject = {
