5000e8a45216a3828c05c3fa6ce2ebad234bd07a
ClintonYounge <clintonyounge18@gmail.com>
2023-02-11 22:06:16 -0400

    Add bugs

--- mobile-menu/script.js
+++ mobile-menu/script.js
@@ -1,14 +1,14 @@
-const menuBtn = document.querySelector('.hamburger');
+const menuBtn = document.querySelector('hamburger');
 const menu = document.getElementById('theMenu');
 const xBtn = document.getElementById('xBtn');
-const navLinks = document.querySelectorAll('.navs');
-const introSection = document.querySelector('.introduction');
+const navLinks = document.querySelectorAll('navs');
+const introSection = document.querySelector('introduction');
 const workSection = document.getElementById('work-section');
 const aboutSection = document.getElementById('about-section');
 const contactSection = document.getElementById('contact');
 
 function openMenu() {
-  menu.style.display = 'block';
+  menu.style.display = block;
   introSection.style.filter = 'blur(2px)';
   workSection.style.filter = 'blur(2px)';
   aboutSection.style.filter = 'blur(2px)';
@@ -16,17 +16,17 @@ function openMenu() {
 }
 
 function closeMenu() {
-  introSection.style.filter = 'none';
-  workSection.style.filter = 'none';
-  aboutSection.style.filter = 'none';
-  contactSection.style.filter = 'none';
-  menu.style.display = 'none';
+  introSection.style.filter = none;
+  workSection.style.filter = none;
+  aboutSection.style.filter = none;
+  contactSection.style.filter = none;
+  menu.style.display = none;
 }
 
-menuBtn.addEventListener('click', openMenu);
+menuBtn.addEventListener(click, openMenu);
 
 for (let i = 0; i < navLinks.length; i += 1) {
-  navLinks[i].addEventListener('click', closeMenu);
+  navLinks[i].addEventListener(click, closeMenu);
 }
 
-xBtn.addEventListener('click', closeMenu);
+xBtn.addEventListener(click, closeMenu);
